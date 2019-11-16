e946492070c8295154272142f478c80a0fb53c76
武田 憲太郎 <takeda@youmind.jp>
2019-11-17 00:21:20 +0900

    コマンド起動モック

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { File } from "./File";
-import { Converter } from "./Converter";
-import fs = require('fs');
-
-const file = new File('./example-orig.png');
-console.log(file.hash, file.size);
-const converter = new Converter('RrpzmMNJkq0z30vdxBb0TGhG16f2llTp');
-
-(async () => {
-  const converted = await converter.convert(file);
-
-  await fs.renameSync(file.path, converted.backup);
-  await fs.writeFileSync(file.path, converted.buffer);
-  console.log(converted);
-  console.log(converted.toCache());
-})()
-
+console.log('hello');
